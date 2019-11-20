de2822ba48253bff4d00ac2217ec80d61a5b25aa
Fabio Luis Pereira <zeta@MacBook-Pro-de-Fabio.local>
2019-11-20 02:01:31 -0300

    variaveis de ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '21a33fda6f1dd4',
-    pass: '33c5786b9a2610',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,4 @@
 export default {
-  dsn: 'https://665e77bd10574f92909b75ce0d026a2d@sentry.io/1827241',
+  // dsn: 'https://665e77bd10574f92909b75ce0d026a2d@sentry.io/1827241',
+  dsn: process.env.SENTRY_DSN,
 };
